31a5c93fde69a238cfd80417fcf5fce35151711e
halaoppah2 <enochoppah2@gmail.com>
2024-08-29 22:12:21 +0000

    practice

--- practice.py
+++ practice.py
@@ -1,1 +1,77 @@
-print("halaoppah")
+print("halaoppah") #This is my name
+
+if 5 > 9:
+    print("Five is greater than nine")
+if 5 < 9:
+    print("Five is less than nine") 
+
+x = 5
+y = 9
+print(x+y)
+
+x = "Oppah"
+x = 5
+print(x)
+
+x = str(3)
+y = int(2)
+z = float(3)
+print(y+z)
+
+x = 5
+y = 'Oppah'
+print(type(x))
+print(type(y))
+
+x, y, z = "Enoch", "Oppah", "Daniel"
+print(x)
+print(y)
+print(z)
+
+Car = ["Range Rover", "Toyota Pick Up", "Benz"]
+x, z, y = Car
+print(z)
+print(y)
+print(x)
+
+x = "Python"
+y = "is"
+z = 9
+print(x, y, z)
+
+x = "Awesome"
+def myFunction():
+    print("Python is " + x)
+myFunction()
+
+x = "Awesome"
+def myFunction():
+    x = "Fantastic"
+    print("Python is " + x)
+myFunction()
+
+print("Python is " + x)
+
+x = 2+1j
+print(x)
+
+x = ["Man", "Woman"]
+print(x)
+
+x = ("Man", "Woman")
+print(x)
+
+y = range(1, 10)
+print(y)
+
+y = {"Age" : 20, "Name" : "Ama"}
+print(y)
+
+x = 10+5j
+print(x)
+
+x = 2
+x = float(x)
+print(x)
+
+
