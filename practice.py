47c3e9568751c4ddff794ba76074d34afb47d01b
halaoppah2 <enochoppah2@gmail.com>
2024-09-04 18:28:23 +0000

    do

--- practice.py
+++ practice.py
@@ -74,4 +74,97 @@ x = 2
 x = float(x)
 print(x)
 
+x = float("3")
+print(x)
 
+print("'OPPAH'")
+
+x = "oppah"
+print(x[3])
+
+for x in "banana":
+    print(x)
+
+x = "banana"
+print(len(x))
+
+txt = "Oppah is learning Python"
+if "Oppah" in txt:
+    print("Oppah is present")
+
+txt = "The man"
+print(txt[-2:-1])
+print(txt.upper())
+print(txt.replace('m', 'M'))
+print(txt.lower())
+print(txt.capitalize())
+
+
+age = 32
+txt = f"My name is oppah and I am {age * 2} years old"
+print(txt)
+
+price = 50
+txt = f"The price of the watch is {price: .2f} pounds"
+print(txt)
+
+x = "hello"
+print(bool(x))
+print(bool(5))
+print(isinstance(x, str))
+
+print(x := 3)
+
+thelist = ["me", 5, "when", "2.0", "see"]
+thelist1 = [2, 5, 8]
+thelist.extend(thelist1)
+thelist[1] = "see"
+thelist.append("it")
+thelist.insert(3, "in")
+print(thelist)
+print(len(thelist))
+print(thelist[-2])
+print(thelist[2:4])
+print(thelist)
+thelist.remove("see")
+print(thelist)
+thelist.pop()
+print(thelist)
+del thelist[3]
+print(thelist)
+thelist.clear()
+print(thelist)
+
+thelist = ["me", 5, "when", "2.0", "see"]
+for x in thelist:
+    print(x)
+
+thelist = ["me", 5, "when", "2.0", "see"]
+for i in range(len (thelist)):
+    print(thelist[i])
+
+thislist = ["apple", "banana", "cherry"]
+i = 0
+while i < len(thislist):
+    print(thislist[i])
+    i = i + 1
+
+thelist = ["me", "when", "see"]
+thelist.sort(reverse=True)
+print(thelist)
+
+thelist = ["me", "when", "see"]
+mylist = thelist.copy()
+print(mylist)
+
+thelist = ("me",)
+print(type(thelist))
+
+thislist = ("me", "You")
+y = list(thislist)
+y.append("Sex")
+print(y) 
+
+(me, You) = thislist
+print(me)
+print(You) 
